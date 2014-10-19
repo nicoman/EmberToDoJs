df5ea9735f442fbe49c4a850f22297bf94b500bd
Marcelo Nicolas Manso <info@nicolasmanso.com.ar>
2014-10-19 10:53:00 -0300

    Add button to remove all completed todo

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -20,7 +20,13 @@ Todos.TodosController = Ember.ArrayController.extend({
 
         // Save the new model
         todo.save();
-        }
+        },
+        
+        clearCompleted: function(){
+            var completed = this.filterBy('isCompleted', true);
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
+        },
     },
     
     remaining: function(){
@@ -30,5 +36,13 @@ Todos.TodosController = Ember.ArrayController.extend({
     inflection: function(){
         var remaining = this.get('remaining');
         return remaining === 1 ? 'item' : 'items';
-    }.property('remaining')
+    }.property('remaining'),
+    
+    hasCompleted: function() {
+        return this.get('completed') > 0;
+    }.property('completed'),
+    
+    completed: function() {
+        return this.filterBy('isCompleted', true).get('length');
+    }.property('@each.isCompleted'),
 });

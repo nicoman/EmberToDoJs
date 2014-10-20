7488e288e403a989cf66b955d47e3a89adc5031a
Marcelo Nicolas Manso <info@nicolasmanso.com.ar>
2014-10-19 21:16:54 -0300

    Finish the tutorial

--- js/application.js
+++ js/application.js
@@ -4,3 +4,8 @@ window.Todos = Ember.Application.create();
 // Add the adapter to connect with fixtures (example data)
 
 Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+// Add real persistence
+//Todos.ApplicationAdapter = DS.LSAdapter.extend()({
+//        namespace: 'todos-emberjs'
+//});
--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -45,4 +45,14 @@ Todos.TodosController = Ember.ArrayController.extend({
     completed: function() {
         return this.filterBy('isCompleted', true).get('length');
     }.property('@each.isCompleted'),
+    
+    allAreDone: function(key, value) {
+        if (value === undefined) {
+            return !!this.get('length') && this.isEvery('isCompleted');
+        } else {
+            this.setEach('isCompleted', value);
+            this.invoke('save');
+            return value;
+        }
+    }.property('@each.isCompleted'),
 });
